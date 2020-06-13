870ea7cb76e98c1049d69246fef221933152480c
ZainabAlawadh <zainabalawadh0@gmail>
2020-06-13 23:17:58 +0300

    done

---
+++ 1/CW-1-Zainab  Alawadh-244.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+let name = "zainab"
+var hobby1 = "cooking"
+var hobby2 = "swimming"
+var age = 17
+var height = 156
+print(name)
+print(hobby1)
+print(hobby2)
+print(age)
+print(height)
+
+let MyFriendName = "noor"
+var MyFriendHobby1 = "reading"
+var MyFriendHobby2 = "swimming"
+var MyFriendAge = 17
+var MyFriendHeight = 158
+
+MyFriendName != name
+MyFriendAge == age
+MyFriendHeight > height
+MyFriendHobby1 != hobby1
+MyFriendHobby2 == hobby2
+
+var ageDifference = age - MyFriendAge
+
